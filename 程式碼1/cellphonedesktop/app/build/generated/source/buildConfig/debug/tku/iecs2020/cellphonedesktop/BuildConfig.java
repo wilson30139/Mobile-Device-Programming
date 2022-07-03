5bc3ed0b116b2a82aaacb86902ecadda9cbc08ce
wilson30139 <43327415+wilson30139@users.noreply.github.com>
2022-07-04 02:16:10 +0800

    Add files via upload

---
+++ 程式碼1/cellphonedesktop/app/build/generated/source/buildConfig/debug/tku/iecs2020/cellphonedesktop/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tku.iecs2020.cellphonedesktop;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "tku.iecs2020.cellphonedesktop";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
