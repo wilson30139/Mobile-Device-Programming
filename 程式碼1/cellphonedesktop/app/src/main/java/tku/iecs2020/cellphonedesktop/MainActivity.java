c9a8658d50b6394641a6fbd27adb318c69e89591
wilson30139 <43327415+wilson30139@users.noreply.github.com>
2022-07-04 02:14:57 +0800

    Add files via upload

---
+++ 程式碼1/cellphonedesktop/app/src/main/java/tku/iecs2020/cellphonedesktop/MainActivity.java
@@ -0,0 +1,14 @@
+package tku.iecs2020.cellphonedesktop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
